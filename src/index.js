da8eebae7d3783c44d4e36147765874693d81b68
syuchan1005 <syuchan.dev@gmail.com>
2018-10-11 21:08:22 +0900

    :construction: Implement manholemap apis

--- src/index.js
+++ src/index.js
@@ -9,28 +9,25 @@ import Router from 'koa-router';
 import Serve from 'koa-static';
 
 import GraphQL from './GraphQL';
-import db from '../db/models/index';
 
-db.sequelize.sync().then(() => {
-  const schemaText = fs.readFileSync(path.join(__dirname, '..', 'schema.graphql'), 'utf-8');
-  const graphQL = new GraphQL(schemaText, '/api', db, console.log);
+const schemaText = fs.readFileSync(path.join(__dirname, '..', 'schema.graphql'), 'utf-8');
+const graphQL = new GraphQL(schemaText, '/api');
 
-  const app = new Koa();
-  app.use(koaCors());
-  app.use(koaBody());
+const app = new Koa();
+app.use(koaCors());
+app.use(koaBody());
 
-  const router = new Router();
+const router = new Router();
 
-  router.post('/api', graphQL.middleware());
-  router.get('/api', graphQL.middleware());
-  router.get('/api/graphiql', graphQL.graphiql);
+router.post('/api', graphQL.middleware());
+router.get('/api', graphQL.middleware());
+router.get('/api/graphiql', graphQL.graphiql);
 
-  app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
-  app.use(Serve(path.join(__dirname, '..', 'static')));
+app.use(Serve(path.join(__dirname, '..', 'static')));
 
-  const port = process.env.PORT || 8080;
-  app.listen(port, () => {
-    console.log(`listen http://localhost:${port}`);
-  });
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`listen http://localhost:${port}`);
 });
