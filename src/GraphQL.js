ed7a152c692d2b8ec133e65a94524852b5e24122
syuchan1005 <syuchan.dev@gmail.com>
2018-10-13 12:17:05 +0900

    :construction: geolocation scheme String -> Float

--- src/GraphQL.js
+++ src/GraphQL.js
@@ -33,28 +33,23 @@ export default class GraphQL {
       nearEntitiesInPoint: async (obj, {
         point, distance, limit, offset,
       }) => {
-        point.lat = Number(point.lat);
-        point.lng = Number(point.lng);
         const manhole = await ManholeMapAPI.nearEntitiesInPoint(point, distance, limit, offset);
-        return manhole;
-      },
-      nearEntities: async (obj, {
-        categoryId, id, distance, limit, offset,
-      }) => {
-        let point;
-        if (categoryId === Database.categories.indexOf('Manhole')) {
-          const entity = ManholeMapAPI.entity(id);
-          if (entity) point = entity.geo;
-        }
-        if (!point) return [];
-        const manhole = ManholeMapAPI.nearEntitiesInPoint(point, distance, limit, offset);
         return manhole;
       },
       searchEntities: async (obj, {
         name, limit, offset,
       }) => {
-        const manhole = ManholeMapAPI.searchEntities(name, limit, offset);
+        const manhole = await ManholeMapAPI.searchEntities(name, limit, offset);
         return manhole;
+      },
+      nearEntities: async (obj, {
+        categoryId, id, distance, limit, offset,
+      }) => {
+        const entity = await GraphQL.Query.entity({}, { categoryId, id });
+        if (!entity) return [];
+        return GraphQL.Query.nearEntitiesInPoint({}, {
+          point: entity.geo, distance, limit, offset,
+        });
       },
       categories: (obj, { limit, offset }) => Database.categories
         .map((name, id) => ({ id, name })).slice(offset, offset + limit),
